6dcc4800a7f9b56cdd0e48b914cbe3c618738043
unknown <TCSSEREN@TT5671508.turkcell.entp.tgc>
2015-02-20 09:46:10 +0200

    hw2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+         dm <- dim(x)
+         im <- matrix(nrow=dm[1],ncol=dm[2])
+         set <- function(y = matrix()) {
+                 x <<- y
+                 dm <- dim(x)
+                 im <<- matrix(nrow=dm[1],ncol=dm[2])
+         }
+         get <- function() x
+         setinv <- function(inv) im <<- inv
+         getinv <- function() im
+         list(set = set, get = get,
+              setinv = setinv,
+              getinv = getinv)
 }
 
 
@@ -12,4 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+                      im <- x$getinv()
+              if(all(!is.na(im))) {
+                          message("getting cached inverse")
+                          return(im)
+                      }
+              data <- x$get()
+              message("calculating inverse")
+              im <- solve(data, ...)
+              x$setinv(im)
+              im
 }
